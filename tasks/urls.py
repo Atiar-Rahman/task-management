5371683b2c5da007d63da5e62a46cef6cee02ac7
Atiar-Rahman <atiar1709@gmail.com>
2025-06-21 19:55:54 +0600

    dynamic url implemented

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from tasks.views import showTask
+from tasks.views import showTask,showSpecific_task
 
 urlpatterns = [
-    path('show-task/',showTask),    
+    path('show-task/',showTask), 
+    path('show-task/<int:id>',showSpecific_task)   
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -7,3 +7,6 @@ def Home(request):
 
 def showTask(request):
     return HttpResponse('show all task')
+
+def showSpecific_task(request,id):
+    return HttpResponse(f"this is specific task page: {id}")
